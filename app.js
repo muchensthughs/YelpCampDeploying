bd770f38a114a82c754e2751f3b8d005fca0b9f1
root <chenmu5898@gmail.com>
2019-07-23 00:40:38 +0000

    change port config

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server Started!");
 });
